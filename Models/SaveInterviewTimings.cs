a36f60ac75c12260af39525d5c4b95d5ddb5a7fc
hemanthreddy-hr <hemanthveluri123@gmail.com>
2022-08-10 15:13:04 +0530

    first commit

---
+++ Models/SaveInterviewTimings.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InterviewSchedulingProject1.Models
+{
+    public class SaveInterviewTimings
+    {
+        public int? Candidateid { get; set; }
+        [Required(ErrorMessage = "The Selection is required")]
+
+        public bool? selected { get; set; }
+        [Display(Name = "Company Name")]
+
+
+        [MaxLength(1000)]
+
+        
+        public string? Company { get; set; }
+        
+        public System.DateTime? InterviewTiming { get; set; }
+    }
+}
